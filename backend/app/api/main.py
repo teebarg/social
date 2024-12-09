6ffaffeb785840a09eae86abb643f36df2a6c381
Adeniyi Aderounmu <33498241+teebarg@users.noreply.github.com>
2024-12-09 14:06:44 +0100

    Implement Push notification (#2)
    * Implement Push notification

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import drafts, items, login, twitter, users, utils
+from app.api.routes import drafts, items, login, twitter, users, utils, notification
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -9,3 +9,4 @@ api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(twitter.router, prefix="/twitter", tags=["twitter"])
 api_router.include_router(drafts.router, prefix="/drafts", tags=["drafts"])
+api_router.include_router(notification.router, prefix="/notifications", tags=["notifications"])
